e2b077fa1c7e4512226f5f5a44f6ec051c8bcc54
Jack Case <jackacase@gmail.com>
2018-02-20 23:18:06 -0500

    starting from scratch

--- skills/hears_general.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(controller) {
-    var titleCase = require('title-case');
-
-    controller.hears(['[Gg]eneral \\w+', '[Mm]ajor \\w+'], 'ambient', function(bot, message) {
-        msString = titleCase(message.match.toString());
-        bot.reply(message, msString + "! *SALUTES*");
-    });
-};
